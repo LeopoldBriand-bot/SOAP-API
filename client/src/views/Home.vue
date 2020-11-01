c09f9b878497496aa3e9ca3d7ed3a8d7c3d4e7e7
Leopold <leopold.briand@laposte.net>
2020-11-01 11:46:23 +0100

    feat(client): init vue client

---
+++ client/src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <div class="home">
+      <v-expansion-panels>
+        <v-expansion-panel
+          v-for="(item,i) in 5"
+          :key="i"
+        >
+          <v-expansion-panel-header>
+            Item
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </div>
+  </v-app>
+</template>
+
+<script>
+import Book from '../components/Book.component';
+import SOAPHelper from '../plugins/SOAPHelper';
+
+export default {
+  name: "Home",
+  components: {
+    Book
+  },
+  computed: {
+    
+  }
+};
+</script>
